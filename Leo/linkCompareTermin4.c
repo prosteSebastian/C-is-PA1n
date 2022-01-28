088b4112d6ec99caacfc031394179db03bb30c0b
Le Vu Neumann <neumalev@fit.cvut.cz>
2022-01-28 22:14:04 +0100

    Leo: linkCompareTermin4.c

--- Leo/linkCompareTermin4.c
+++ Leo/linkCompareTermin4.c
@@ -42,10 +42,9 @@ int checkSizes ( Tnode ** a, int nr ) {
   Tnode * tmp1;
   for ( int i = 0 ; i < nr ; ++i ) {
     tmp1 = a[i];
-    for ( ; tmp1->m_Next != NULL ; tmp1 = tmp1->m_Next ) {
+    for ( ; tmp1->m_Next != NULL ; tmp1 = tmp1->m_Next )
       if ( tmp1->m_Val > tmp1->m_Next->m_Val )
         return 1;
-    }
   }
   return 0;
 }
@@ -55,6 +54,63 @@ void delArr ( Tnode ** a, int nr ) {
     delList ( a[i] );
 }
 
+Tnode * resList ( Tnode * a, Tnode * b ) {
+  Tnode * res = a, * tmp, * head;
+  int mode = 0; //on a[0]/a branch
+
+  if ( a->m_Val > b->m_Val ) {
+    mode = 1; //on a[i]/b branch 
+    res = b;
+  }
+
+  head = res;
+
+  while ( a != NULL || b!= NULL ) {
+    if ( mode ) { //on a[i]/b branch
+      if ( res->m_Next == NULL && a != NULL ) {
+        res->m_Next = a;
+        break;
+      }
+      else if ( a == NULL ) {
+        break;
+      }
+      else if ( res->m_Next->m_Val <= a->m_Val ) {
+        res = res->m_Next;
+        b = b->m_Next;
+      }
+      else if ( res->m_Next->m_Val > a->m_Val ) {
+        tmp = b->m_Next;
+        res->m_Next = a;
+        mode = 0;
+        b = tmp;
+        res = res->m_Next;
+      }
+    }
+    else {
+      if ( res->m_Next == NULL && b != NULL ) {
+        res->m_Next = b;
+        break;
+      }
+      else if ( b == NULL ) {
+        break;
+      }
+      else if ( res->m_Next->m_Val <= b->m_Val ) {
+        res = res->m_Next;
+        a = a->m_Next;
+      }
+      else if ( res->m_Next->m_Val > a->m_Val ) {
+        tmp = a->m_Next;
+        res->m_Next = b;
+        mode = 1;
+        a = tmp;
+        res = res->m_Next;
+      }
+    }
+  }
+
+  return head;
+}
+
 Tnode * compareList ( Tnode ** a, int nr ) {
   if ( checkList ( a, nr ) || checkSizes ( a, nr ) ) {
     delArr ( a, nr );
@@ -62,6 +118,14 @@ Tnode * compareList ( Tnode ** a, int nr ) {
   }
 
   Tnode * res = a[0];
+
+  for ( int i = 1 ; i < nr ; ++i ) {
+    res = resList ( res, a[i] );
+  }
+
+  Tnode * headTmp = res;;
+  printList ( headTmp );
+
   return res;
 }
 
@@ -69,31 +133,84 @@ Tnode * compareList ( Tnode ** a, int nr ) {
 int main ( int argc, char * argv [] ) {
   Tnode * a[5];
   Tnode * res;
-  
-  // a[0] = createItem ( 1,
-  //          createItem ( 7,
-  //            createItem ( 24, NULL )));
-  // a[1] = createItem ( 3,
-  //          createItem ( 5,
-  //            createItem ( 17, NULL )));
-  // a[2] = createItem ( -3,
-  //          createItem ( 19, NULL ));
-  // res = compareList ( a, 3 );
-  // delList ( res );
 
-  // a[0] = createItem ( 15,
-  //          createItem ( 19,
-  //            createItem ( 23,
-  //              createItem ( 192, NULL ))));
-  // a[1] = createItem ( 17,
-  //          createItem ( 18,
-  //            createItem ( 18,
-  //              createItem ( 21, NULL ))));
-  // a[2] = createItem ( -3,
-  //         createItem ( 19, NULL ));
-  // res = compareList ( a, 3 );
-  // delList ( res );
+  a[0] = createItem ( 5, NULL );
+  a[1] = createItem ( 1,
+           createItem ( 2,
+             createItem ( 6,
+               createItem ( 7,
+                 createItem ( 8,
+                   createItem ( 9, NULL ))))));
+  res = compareList ( a, 2 );
+  delList ( res );
   
+  a[0] = createItem ( 1,
+           createItem ( 7,
+             createItem ( 24, NULL )));
+  a[1] = createItem ( 3,
+           createItem ( 5,
+             createItem ( 17, NULL )));
+  a[2] = createItem ( -3,
+           createItem ( 19, NULL ));
+  res = compareList ( a, 3 );
+  delList ( res );
+
+  a[0] = createItem ( 15,
+           createItem ( 19,
+             createItem ( 23,
+               createItem ( 192, NULL ))));
+  a[1] = createItem ( 17,
+           createItem ( 18,
+             createItem ( 18,
+               createItem ( 21, NULL ))));
+  a[2] = createItem ( -3,
+          createItem ( 19, NULL ));
+  res = compareList ( a, 3 );
+  delList ( res );
+
+  a[0] = createItem ( -4,
+           createItem ( 2,
+             createItem ( 4,
+               createItem ( 7, NULL ))));
+  a[1] = createItem ( 12, 
+           createItem ( 18, 
+             createItem ( 20, NULL )));
+  a[2] = createItem ( -6, 
+           createItem ( 3, 
+             createItem ( 4,
+               createItem ( 11,
+                 createItem ( 14,
+                   createItem ( 15, 
+                     createItem ( 19, NULL )))))));
+  res = compareList ( a, 3 );
+  delList ( res );
+
+  a[0] = createItem ( -4,
+           createItem ( 2,
+             createItem ( 4,
+               createItem ( 7, NULL ))));
+  a[1] = createItem ( 12, 
+           createItem ( 18, 
+             createItem ( 20, NULL )));
+  a[2] = createItem ( -6, 
+           createItem ( 3, 
+             createItem ( 4,
+               createItem ( 11,
+                 createItem ( 1,
+                   createItem ( 15, 
+                     createItem ( 19, NULL )))))));
+  res = compareList ( a, 3 );
+  assert ( res == NULL );
+
+  a[0] = createItem ( -4,
+           createItem ( 2,
+             createItem ( 4,
+               createItem ( 7, NULL ))));
+  a[1] = createItem ( 12, NULL );
+  a[2] = createItem ( 3, NULL );
+  res = compareList ( a, 3 );
+  delList ( res );
+
   a[0] = createItem ( 15,
            createItem ( 19,
              createItem ( 19,
@@ -120,5 +237,3 @@ int main ( int argc, char * argv [] ) {
   return 0;
 }
 #endif /* __PROGTEST__ */
-
-
